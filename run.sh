4c8022c8d4ca883ad8e7fec9e4e0aa3d31dda43d
Eduardo <86479444+L0G1C06@users.noreply.github.com>
2022-08-17 17:17:35 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#exemplo de como executar esse script: python webcam_object_detection.py
+# aperte "q" para encerrar
+cd /home/brain/projects/yolov6/ONNX-YOLOv6-Object-Detection
+source /home/brain/projects/yolov6/ONNX-YOLOv6-Object-Detection/env/bin/activate
+python /home/brain/projects/yolov6/ONNX-YOLOv6-Object-Detection/webcam_object_detection.py
+cd /home/brain/Desktop/demos-emDesenvolvimento
